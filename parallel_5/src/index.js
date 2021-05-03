b1377720b52489da97f70a93fa071c0c73a1b3e2
Alexandre Andrade <aleandrade@gmail.com>
2021-05-03 12:01:04 +1000

    feat: 🎸 add more two containers

---
+++ parallel_5/src/index.js
@@ -0,0 +1,15 @@
+const joi = require('joi')
+const chalk = require('chalk')
+const mysql = require('mysql')
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+const init = async () => {
+  console.log(process.env)
+  while (1) {
+    await sleep(10000)
+  }
+}
+
+init()
